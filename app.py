407bc985d5cb1f4eb72d0faec518ef021e02d204
Yousuf <43644200+yousuf9292@users.noreply.github.com>
2021-03-15 00:38:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
 from flask import Flask,render_template,redirect,flash,url_for
+from flask_mail import Mail,Message
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import InputRequired,Email
-import smtplib
-from flask_mail import Mail,Message
 
 app=Flask(__name__)
-
-mail = Mail(app)
-
 
 app.config['SECRET_KEY'] = 'mykey'
 app.config['DEBUG']=True
 app.config['TESTING']=False
 app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True   
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME']='yousufsyed900@gmail.com'
 app.config['MAIL_PASSWORD']='Kiop000!'
 app.config['MAIL_MAX_EMAILS']=None
@@ -25,20 +21,10 @@ app.config['MAIL_ASCII_ATTACHMENTS']=False
 mail = Mail(app)
 
 
-
 class MyForm(FlaskForm):
     body = StringField('Message',validators=[InputRequired(message="Message Is Required")])
     name = StringField('Name',validators=[InputRequired(message="Name Is Required")])
     email=StringField('Email',validators=[InputRequired(),Email(message='Email Format is Required')])
-
-    
-    
-
-
-
-
-
-
 
 
 
@@ -46,14 +32,12 @@ class MyForm(FlaskForm):
 def home():
     form=MyForm()
     if form.validate_on_submit():
+        print(form.email.data)
         msg =Message(sender="yousufsyed900@gmail.com",reply_to=form.email.data,subject=form.email.data,recipients=['yousufsyed900@gmail.com'],body=form.body.data)
-        session = smtplib.SMTP('smtp.gmail.com', 587) 
-        session.starttls()
-        session.login('yousufsyed900@gmail.com', 'Kiop000!')
         mail.send(msg)
         reply=Message(sender="yousufsyed900@gmail.com",subject="Asalam U ALikum",body="How are you "+str(form.name.data)+", It is a message from syed yousuf here is my freelance work https://www.fiverr.com/users/syedyousuf90/manage_orders?source=header_navigation&search_type=completed",recipients=[form.email.data])
         mail.send(reply)
-        session.quit()
+
         flash("Check Your Email "+str(form.name.data))
         return redirect(url_for('home'))
     return render_template('index.html',form=form)
