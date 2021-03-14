b93d599507909ef3abd385e55855ea45d727f889
Yousuf <43644200+yousuf9292@users.noreply.github.com>
2021-03-15 00:02:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
 from flask import Flask,render_template,redirect,flash,url_for
-from flask_mail import Mail,Message
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import InputRequired,Email
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 app=Flask(__name__)
 
-app.config['SECRET_KEY'] = 'mykey'
-app.config['DEBUG']=True
-app.config['TESTING']=False
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME']='yousufsyed900@gmail.com'
-app.config['MAIL_PASSWORD']='Kiop000!'
-app.config['MAIL_MAX_EMAILS']=None
-app.config['MAIL_DEFAULT_SENDER']='yousufsyed900@gmail.com'
-app.config['MAIL_ASCII_ATTACHMENTS']=False
-mail = Mail(app)
 
 
 class MyForm(FlaskForm):
@@ -26,18 +16,38 @@ class MyForm(FlaskForm):
     name = StringField('Name',validators=[InputRequired(message="Name Is Required")])
     email=StringField('Email',validators=[InputRequired(),Email(message='Email Format is Required')])
 
+    
+    
+
+mail_content = '''Hello,This is a simple mail.' ' '
+
+
+message = MIMEMultipart()
+
+
+
+
 
 
 @app.route('/',methods=['GET','POST'])
 def home():
     form=MyForm()
     if form.validate_on_submit():
-        print(form.email.data)
-        msg =Message(sender="yousufsyed900@gmail.com",reply_to=form.email.data,subject=form.email.data,recipients=['yousufsyed900@gmail.com'],body=form.body.data)
-        mail.send(msg)
-        reply=Message(sender="yousufsyed900@gmail.com",subject="Asalam U ALikum",body="How are you "+str(form.name.data)+", It is a message from syed yousuf here is my freelance work https://www.fiverr.com/users/syedyousuf90/manage_orders?source=header_navigation&search_type=completed",recipients=[form.email.data])
-        mail.send(reply)
-
+        message['Subject'] = 'Asalam U Alikum' 
+        sender_address = 'yousufsyed900@gmail.com'
+        sender_pass = 'Kiop000!'
+        receiver_address = form.email.data
+        sender_address = 'yousufsyed900@gmail.com'
+        sender_pass = 'Kiop000!'
+        receiver_address = form.email.data
+        message['From'] = sender_address
+        message['To'] = receiver_address
+        session = smtplib.SMTP('smtp.gmail.com', 587) 
+        session.starttls()
+        session.login(sender_address, sender_pass)
+        text = message.as_string()
+        session.sendmail(sender_address, receiver_address, text)
+        session.quit()
         flash("Check Your Email "+str(form.name.data))
         return redirect(url_for('home'))
     return render_template('index.html',form=form)
